4ab034013c5c4cf6ddcd96bba99af1f64b97a1d2
J1NNYz <122804128+J1NNYz@users.noreply.github.com>
2023-02-14 19:39:26 +0700

    Create GetX.lua

---
+++ GetX.lua
@@ -0,0 +1,21 @@
+{
+  "args": {}, 
+  "headers": {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", 
+    "Accept-Encoding": "gzip, deflate, br", 
+    "Accept-Language": "en-US,en;q=0.9,th;q=0.8", 
+    "Host": "httpbin.org", 
+    "Sec-Ch-Ua": "\"Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"Google Chrome\";v=\"110\"", 
+    "Sec-Ch-Ua-Mobile": "?0", 
+    "Sec-Ch-Ua-Platform": "\"Windows\"", 
+    "Sec-Fetch-Dest": "document", 
+    "Sec-Fetch-Mode": "navigate", 
+    "Sec-Fetch-Site": "none", 
+    "Sec-Fetch-User": "?1", 
+    "Upgrade-Insecure-Requests": "1", 
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36", 
+    "X-Amzn-Trace-Id": "Root=1-63eb80de-6470823a1d117ca208b0c258"
+  }, 
+  "origin": "49.228.250.57", 
+  "url": "https://httpbin.org/get"
+}
